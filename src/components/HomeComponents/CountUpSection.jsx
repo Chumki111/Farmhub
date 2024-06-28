a34e4c61e9f3ee862cd5a5d9ba05efe759619c79
Chumki khatun <chumkikhatun370@gmail.com>
2024-06-28 13:11:19 +0600

    complete gallery section home page

---
+++ src/components/HomeComponents/CountUpSection.jsx
@@ -0,0 +1,90 @@
+import CountUp from 'react-countup';
+import ScrollTrigger from 'react-scroll-trigger';
+import { useState } from "react";
+// images from assets file
+import count1 from '../../assets/count1.svg'
+import count2 from '../../assets/count2.svg'
+import count3 from '../../assets/count3.svg'
+import count4 from '../../assets/count4.svg'
+
+const CountUpSection = () => {
+    const [counter,setCounter] = useState(false)
+  return (
+    <div className="mb-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="bg-white shadow-lg rounded-2xl py-10 px-2">
+    <ScrollTrigger onEnter={() => setCounter(true)} onExit={() => setCounter(false)}>
+        <div className="flex justify-evenly flex-wrap">
+            {/* first card */}
+        
+        <div className="flex justify-center items-center  space-y-5">
+        <div className=''>
+        <h2 className="text-3xl lg:text-5xl lg:font-bold">
+        { counter && <CountUp start={0} end={200} duration={2} delay={0}/>}
+        <span className="text-primary">+</span></h2>
+      <div className="flex flex-col md:flex-row justify-start md:justify-center items-start md:items-center gap-3  space-y-0 md:space-y-4">
+        <img src={count1} alt="" />
+      
+      <h2 className="font-semibold pb-4">Team member</h2>
+        </div>
+      </div>
+      
+      
+
+        </div>
+        {/* second card */}
+        <div className="flex justify-center items-center  space-y-5">
+        <div className=''>
+        <h2 className="text-3xl lg:text-5xl lg:font-bold">
+        { counter && <CountUp start={0} end={300} duration={2} delay={0}/>}
+        <span className="text-primary">K+</span></h2>
+      <div className="flex flex-col md:flex-row justify-start md:justify-center items-start md:items-center gap-3  space-y-2 md:space-y-4">
+        <img src={count2} alt="" />
+      
+      <h2 className="font-semibold pb-4">Winning award</h2>
+        </div>
+      </div>
+      
+      
+
+        </div>
+        {/* third card */}
+        <div className="flex justify-center items-center space-y-5">
+        <div className=''>
+        <h2 className="text-3xl lg:text-5xl lg:font-bold">
+        { counter && <CountUp start={0} end={100} duration={2} delay={0}/>}
+        <span className="text-primary">+</span></h2>
+      <div className="flex flex-col md:flex-row justify-start md:justify-center items-start md:items-center gap-3  space-y-2 md:space-y-4">
+        <img src={count3} alt="" />
+      
+      <h2 className="font-semibold pb-4">Complete project</h2>
+        </div>
+      </div>
+      
+      
+
+        </div>
+        {/* forth card  */}
+        <div className="flex justify-center items-center  space-y-5">
+        <div className=''>
+        <h2 className="text-3xl lg:text-5xl lg:font-bold">
+        { counter && <CountUp start={0} end={900} duration={2} delay={0}/>}
+        <span className="text-primary">+</span></h2>
+      <div className="flex flex-col md:flex-row justify-start md:justify-center items-start md:items-center gap-3  space-y-2 md:space-y-4">
+        <img src={count4} alt="" />
+      
+      <h2 className="font-semibold pb-4">Client review</h2>
+        </div>
+      </div>
+      
+      
+
+        </div>
+        
+        </div>
+        </ScrollTrigger>
+    </div>
+</div>
+  )
+}
+
+export default CountUpSection
--- src/components/HomeComponents/GallarySection.jsx
+++ src/components/HomeComponents/GallarySection.jsx
@@ -1,54 +1,60 @@
-
-
+// images from file
 import image1 from '../../assets/gl1.jpg'
 import image2 from '../../assets/gl2.jpg'
 import image3 from '../../assets/gl3.jpg'
 import image4 from '../../assets/gl4.jpg'
 import image5 from '../../assets/gl5.jpg'
+import CountUpSection from './CountUpSection'
+
 
 
 const GallarySection = () => {
-   
-  return (
-    <>
-<div className="flex flex-col mt-10">
-    <div className="bg-[#EFF3ED] ">
-<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <div className='py-14'>
-    <div className='text-center'>
-        <span className='text-secondary text-lg font-medium text-center'>OUR GALLARY</span>
-        <h1 className='text-4xl md:text-4xl lg:text-6xl font-semibold lg:leading-[60px] pt-4 text-primary whitespace-pre-line max-w-2xl mx-auto'>
-    Bringing nature's bounty
-    to your plate
-</h1>
-    </div>
-    <div className="flex flex-col lg:flex-row gap-5 pt-10">
-        <img src={image1} alt="" className='rounded-2xl'/>
-        <img src={image2} alt="" className='rounded-2xl bg-cover '/>
-    </div>
-    <div className="flex flex-wrap md:flex-wrap justify-evenly gap-3 md:gap-0 pt-8">
-        <img src={image3} alt="" className='h-[425px] rounded-2xl'/>
-        <img src={image4} alt="" className='h-[425px] rounded-2xl'/>
-        <div className="mt-3">
-        <img src={image5} alt="" className='h-[350px] rounded-2xl'/>
-       <div className='mt-4'>
-       <button className="relative text-xl bg-secondary text-white py-3 px-10 rounded-2xl overflow-hidden group hover:text-primary w-full">
-                  <span className="relative z-10 flex items-center justify-center">
-                    <span>View All Photo</span>
-                    
-                  </span>
-                  <div className="absolute inset-0 bg-third transition-all duration-300 ease-in-out transform scale-x-0 scale-y-0 group-hover:scale-x-100 group-hover:scale-y-100 origin-center"></div>
-                </button>
-       </div>
-        </div>
-    </div>
-    
-    </div>
-</div>
-    </div>
-</div>
-    </>
-  )
+
+    return (
+        <>
+            <div className="flex flex-col mt-10 relative">
+                <div className="bg-[#EFF3ED] ">
+                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div className='pt-14 pb-28'>
+                            <div className='text-center'>
+                                <span className='text-secondary text-lg font-medium text-center'>OUR GALLARY</span>
+                                <h1 className='text-4xl md:text-4xl lg:text-6xl font-semibold lg:leading-[60px] pt-4 text-primary whitespace-pre-line max-w-2xl mx-auto'>
+                                    Bringing nature's bounty
+                                    to your plate
+                                </h1>
+                            </div>
+                            <div className="flex flex-col lg:flex-row gap-5 pt-10">
+                                <img src={image1} alt="" className='rounded-2xl' />
+                                <img src={image2} alt="" className='rounded-2xl bg-cover ' />
+                            </div>
+                            <div className="flex flex-wrap md:flex-wrap justify-evenly gap-3 md:gap-0 pt-8">
+                                <img src={image3} alt="" className='h-[425px] rounded-2xl' />
+                                <img src={image4} alt="" className='h-[425px] rounded-2xl' />
+                                <div className="mt-3">
+                                    <img src={image5} alt="" className='h-[350px] rounded-2xl' />
+                                    {/* button */}
+                                    <div className='mt-4'>
+                                        <button className="relative text-xl bg-secondary text-white py-3 px-10 rounded-2xl overflow-hidden group hover:text-primary w-full">
+                                            <span className="relative z-10 flex items-center justify-center">
+                                                <span>View All Photo</span>
+
+                                            </span>
+                                            <div className="absolute inset-0 bg-third transition-all duration-300 ease-in-out transform scale-x-0 scale-y-0 group-hover:scale-x-100 group-hover:scale-y-100 origin-center"></div>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                {/* count section */}
+                <div className='relative bottom-24'>
+                    <CountUpSection/>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default GallarySection
