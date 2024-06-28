3fc9fde90bb6b20f5232b541c870963b26bc1be7
Chumki khatun <chumkikhatun370@gmail.com>
2024-06-28 07:42:07 +0600

    complete why choose us sectopn

--- src/components/HomeComponents/ChooseUsSection.jsx
+++ src/components/HomeComponents/ChooseUsSection.jsx
@@ -11,19 +11,19 @@ const ChooseUsSection = () => {
   return (
     <div className="flex flex-col mt-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-col md:flex-row justify-between">
+        <div className="flex flex-col lg:flex-row justify-between">
           {/* image section */}
-          <div className="relative">
+          <div className="relative mt-6 lg:mt-0">
             {/* Main image */}
             <img src={chooseMan} alt="Choose Man" className="block pt-5 md:w-[90%]" />
 
-            <img src={lFood} alt="Left Food" className="absolute top-0 left-28 transform -translate-x-8" />
-            <img src={fFood} alt="First Food" className="absolute bottom-40  transform -translate-x-8" />
-            <img src={tFood} alt="Third Food" className="absolute bottom-4 left-20 transform translate-y-6" />
+            <img src={lFood} alt="Left Food" className="absolute top-0 left-28 transform md:-translate-x-8 w-16 md:w-32" />
+            <img src={fFood} alt="First Food" className="absolute bottom-40  transform md:-translate-x-8 w-16 md:w-40" />
+            <img src={tFood} alt="Third Food" className="absolute bottom-4 left-20 transform md:translate-y-6 w-16 md:w-36" />
 
           </div>
           {/* content section */}
-          <div className="">
+          <div className="order-first lg:order-last">
             <span className='text-secondary text-lg font-medium'>Why Chose Us</span>
             <h1 className='text-4xl md:text-4xl lg:text-6xl font-semibold md:leading-[85px] pt-4 text-primary'>Bringing nature's <br /> bounty to your plate</h1>
             <p className='pt-4 text-zinc-400 max-w-md'>Agriculture and farming are essential industries that involve the cultivation of crops, raising of livestock, and production</p>
