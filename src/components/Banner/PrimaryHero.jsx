c52bf6047c4e55bc12c8f5105747ea66fba38c9b
Chumki khatun <chumkikhatun370@gmail.com>
2024-07-02 07:11:14 +0600

    complete about us section

---
+++ src/components/Banner/PrimaryHero.jsx
@@ -0,0 +1,35 @@
+import backgroundImage from '../../assets/bread-bg.png'
+import image from '../../assets/tt-slice.png'
+import { IoIosArrowForward } from "react-icons/io";
+const PrimaryHero = ({text}) => {
+  return (
+    <div className='flex flex-col'>
+      <div 
+        className="relative text-white"
+        style={{ backgroundImage: `linear-gradient(to right, rgba(31, 78, 61, 0.7), rgba(31, 78, 61, 0.7)), url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+      >
+        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+          <div className='flex flex-col md:flex-row justify-between mt-16 pb-16'>
+            {/* content section */}
+            <div className='pt-8 md:pt-10'>
+              <h1 className='text-4xl md:text-4xl lg:text-7xl font-semibold md:leading-[85px]'>About Us</h1>
+              
+              
+              <div className="flex space-x-3 items-center pt-5">
+                <h2 className='text-xl'>Home</h2>
+                <IoIosArrowForward className='text-xl'/>
+                <h2 className='text-xl'>{text}</h2>
+              </div>
+            </div>
+            {/* image section */}
+            <div className='pt-4 md:pt-0'>
+              <img src={image} alt="" className='animate-pulse' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PrimaryHero
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,23 @@
+import PrimaryHero from "../components/Banner/PrimaryHero"
+import ChooseUsSection from "../components/HomeComponents/ChooseUsSection"
+import CompanySection from "../components/HomeComponents/CompanySection"
+import MessageSection from "../components/HomeComponents/MessageSection"
+import ServicesSection from "../components/HomeComponents/ServicesSection"
+import Testimonial from "../components/HomeComponents/Testimonial"
 
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <PrimaryHero text={'About Us'}/>
+    <ChooseUsSection/>
+    <div className="mt-16">
+    <ServicesSection/>
+    </div>
+    <Testimonial/>
+    <MessageSection/>
+    <CompanySection/>
+    </>
   )
 }
 
