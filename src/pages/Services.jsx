12a37b8c24020e95096e53b4a7adfa273e5a8383
Chumki khatun <chumkikhatun370@gmail.com>
2024-07-03 08:41:45 +0600

    get all servicesuse tanstack query

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,7 +1,14 @@
+import { useQuery } from "@tanstack/react-query"
 import PrimaryHero from "../components/Banner/PrimaryHero"
+import { getAllServices } from "../api/service"
 
 
 const Services = () => {
+  const {data:services} = useQuery({
+    queryKey:['services'],
+    queryFn:async() => getAllServices()
+  })
+  console.log(services);
   return (
     <>
     <PrimaryHero text={"Services"}/>
