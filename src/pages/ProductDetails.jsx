246a6bc553701a7c2bb9d1832e91bea0024d7133
Chumki khatun <chumkikhatun370@gmail.com>
2024-07-06 20:34:26 +0600

    complete products page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -13,7 +13,7 @@ const ProductDetails = ({product}) => {
       <div className="flex flex-col justify-center items-start pl-4 mt-4 md:mt-0 ml-4">
         <h2 className="text-lg font-medium">{product.name}</h2>
         <p className="text-sm max-w-xs text-gray-600 mt-2">{product.description}</p>
-        <p className="text-md max-w-sm font-semibold mt-2">${product.price}</p>
+        <p className="text-xl text-secondary  font-semibold mt-2">${product.price}</p>
         <div className="my-4">
         <button className="px-4 py-2 border border-[#EFF3ED] rounded-full group-hover:bg-secondary group-hover:text-white transition duration-200">Add To Cart</button>
         </div>
